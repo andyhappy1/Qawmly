c4e6641168c4e16a6f6dd4a5060b49aa8ec339c3
Susanne Rossignol <susanne.rossignol@gmail.com>
2014-07-03 00:13:58 -0400

    video add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,12 +28,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
+
     respond_to do |format|
       if @user.save
         # Tell the UserMailer to send a welcome email after save
         UserMailer.welcome_email(@user).deliver
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
+        flash[:success] = "Cool. See you soon."
+        format.html { redirect_to(:back) }
+       #format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
